fa5fdb7c8b19e9e071a9579f5304b44718b4e1ab
Ajanitone <ajanitone@freenet.de>
2022-07-07 11:29:01 +0200

    exercise strings & numbers

---
+++ solution.js
@@ -0,0 +1,17 @@
+let introSentence = "hello, my name is Fran and I am";
+let age = 25;
+
+// /the + operand adds both declarations together/
+const statement = introSentence + age;
+
+console.log(`01: the statement is: ${statement}`);
+
+console.log("----------------------------------------");
+
+const interger = "1005";
+console.log("02a: Convert a String to a Number", parseInt(interger));
+
+console.log("----------------------------------------");
+
+const float = "10.05";
+console.log("02b: Convert a String-float to a Number", parseFloat(float));
